88a622bb0116887a2dcefa06ffb6e654aa6dc20b
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2016-07-03 18:08:46 -0300

    Documented new interface to prop/T integrals

--- thermo/utils.py
+++ thermo/utils.py
@@ -1397,11 +1397,58 @@ class TDependentProperty(object):
                 pass
         return None
 
-
     def calculate_integral_over_T(self, T1, T2, method):
+        r'''Method to calculate the integral of a property over temperature
+        with respect to temperature, using a specified method. Uses SciPy's 
+        `quad` function to perform the integral, with no options.
+        
+        This method can be overwritten by subclasses who may perfer to add
+        analytical methods for some or all methods as this is much faster.
+
+        If the calculation does not succeed, returns the actual error
+        encountered.
+
+        Parameters
+        ----------
+        T1 : float
+            Lower limit of integration, [K]
+        T2 : float
+            Upper limit of integration, [K]
+        method : str
+            Method for which to find the integral
+
+        Returns
+        -------
+        integral : float
+            Calculated integral of the property over the given range, 
+            [`units`]
+        '''
         return float(quad(lambda T: self.calculate(T, method)/T, T1, T2)[0])
 
     def T_dependent_property_integral_over_T(self, T1, T2):
+        r'''Method to calculate the integral of a property over temperature 
+        with respect to temperature, using a specified method. Methods found
+        valid by `select_valid_methods` are attempted until a method succeeds. 
+        If no methods are valid and succeed, None is returned.
+        
+        Calls `calculate_integral_over_T` internally to perform the actual
+        calculation.
+
+        Parameters
+        ----------
+        T1 : float
+            Lower limit of integration, [K]
+        T2 : float
+            Upper limit of integration, [K]
+        method : str
+            Method for which to find the integral
+
+        Returns
+        -------
+        integral : float
+            Calculated integral of the property over the given range, 
+            [`units`]
+        '''
         sorted_valid_methods = self.select_valid_methods(T1)
         for method in sorted_valid_methods:
             try:
