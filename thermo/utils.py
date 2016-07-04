a5da7eaa8b69024fc6eb39089ac5952085dc8741
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2016-07-04 19:20:00 -0300

    Added documentation for plotting isotherms and isobars

--- thermo/utils.py
+++ thermo/utils.py
@@ -1836,6 +1836,34 @@ class TPDependentProperty(TDependentProperty):
 
     def plot_isotherm(self, T, Pmin=None, Pmax=None, methods_P=[], pts=50,
                       only_valid=True):  # pragma: no cover
+        r'''Method to create a plot of the property vs pressure at a specified
+        temperature according to either a specified list of methods, or the 
+        user methods (if set), or all methods. User-selectable number of 
+        points, and pressure range. If only_valid is set,
+        `test_method_validity_P` will be used to check if each temperature in 
+        the specified range is valid, and `test_property_validity` will be used
+        to test the answer, and the method is allowed to fail; only the valid 
+        points will be plotted. Otherwise, the result will be calculated and 
+        displayed as-is. This will not suceed if the method fails.
+
+        Parameters
+        ----------
+        Pmin : float
+            Minimum pressure, to begin calculating the property, [Pa]
+        Pmax : float
+            Maximum pressure, to stop calculating the property, [Pa]
+        methods : list, optional
+            List of methods to consider
+        pts : int, optional
+            A list of points to calculate the property at; if Pmin to Pmax
+            covers a wide range of method validities, only a few points may end
+            up calculated for a given method so this may need to be large
+        only_valid : bool
+            If True, only plot successful methods and calculated properties,
+            and handle errors; if False, attempt calculation without any
+            checking and use methods outside their bounds
+        '''
+        # This function cannot be tested
         if Pmin is None:
             if self.Pmin is not None:
                 Pmin = self.Pmin
@@ -1877,6 +1905,36 @@ class TPDependentProperty(TDependentProperty):
 
     def plot_isobar(self, P, Tmin=None, Tmax=None, methods_P=[], pts=50,
                     only_valid=True):  # pragma: no cover
+        r'''Method to create a plot of the property vs temperature at a 
+        specific pressure according to
+        either a specified list of methods, or user methods (if set), or all
+        methods. User-selectable number of points, and temperature range. If
+        only_valid is set,`test_method_validity_P` will be used to check if 
+        each temperature in the specified range is valid, and
+        `test_property_validity` will be used to test the answer, and the
+        method is allowed to fail; only the valid points will be plotted.
+        Otherwise, the result will be calculated and displayed as-is. This will
+        not suceed if the method fails.
+
+        Parameters
+        ----------
+        P : float
+            Pressure for the isobar, [Pa]
+        Tmin : float
+            Minimum temperature, to begin calculating the property, [K]
+        Tmax : float
+            Maximum temperature, to stop calculating the property, [K]
+        methods : list, optional
+            List of methods to consider
+        pts : int, optional
+            A list of points to calculate the property at; if Tmin to Tmax
+            covers a wide range of method validities, only a few points may end
+            up calculated for a given method so this may need to be large
+        only_valid : bool
+            If True, only plot successful methods and calculated properties,
+            and handle errors; if False, attempt calculation without any
+            checking and use methods outside their bounds
+        '''
         if Tmin is None:
             if self.Tmin is not None:
                 Tmin = self.Tmin
