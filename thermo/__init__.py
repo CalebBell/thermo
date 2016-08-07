28f4fe35e14d16747e740ad31b7b7b14b40c23bd
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2016-08-07 13:15:20 -0300

    Release 0.1.15 for RTD

--- thermo/__init__.py
+++ thermo/__init__.py
@@ -94,4 +94,4 @@ __all__ = ['activity', 'chemical', 'combustion', 'critical',
  'thermal_conductivity', 'triple', 'utils',
  'vapor_pressure', 'virial', 'viscosity', 'volume']
 
-__version__ = '0.1.14'
+__version__ = '0.1.15'
