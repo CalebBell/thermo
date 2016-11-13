687f182463ea65a5d8269461e3779c95be14611d
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2016-11-13 00:57:03 -0400

    Updated coolprop test

--- tests/test_coolprop.py
+++ tests/test_coolprop.py
@@ -27,7 +27,9 @@ from thermo.identifiers import checkCAS
 
 def test_fluid_props():
     tots = [sum([getattr(f, prop) for f in coolprop_fluids.values()]) for prop in ['Tmin', 'Tmax', 'Pmax', 'Tc', 'Pc', 'Tt', 'omega']]
-    tots_exp = [18589.301, 71673.0, 30717000000.0, 45189.59849999997, 440796750.0429425, 18589.301, 11.8685953516]
+    tots_exp =  [18589.301, 71575.0, 30817000000.0, 45189.59849999997, 440791794.7987591, 18589.301, 30.90243968446593]
+              
+
     assert_allclose(tots_exp, tots)
 
     assert len(coolprop_fluids) == len(coolprop_dict)
