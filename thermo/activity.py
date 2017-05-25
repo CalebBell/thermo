80c233511088987cb3a5d14da2ff2498efc7e66a
Enrique Cintron <cintronej@gmail.com>
2017-05-25 01:34:14 -0400

    added function to calculate bubble point at a given pressure. preliminary test case added.

--- thermo/activity.py
+++ thermo/activity.py
@@ -26,7 +26,8 @@ __all__ = ['K_value', 'Rachford_Rice_flash_error', 'Rachford_Rice_solution',
            'Li_Johns_Ahmadi_solution', 'flash_inner_loop', 'NRTL', 'Wilson',
            'UNIQUAC', 'flash', 'dew_at_T',
            'bubble_at_T', 'identify_phase', 'mixture_phase_methods',
-           'identify_phase_mixture', 'Pbubble_mixture', 'Pdew_mixture']
+           'identify_phase_mixture', 'Pbubble_mixture', 'bubble_at_P',
+           'Pdew_mixture']
 
 from scipy.optimize import fsolve, newton, brenth
 from thermo.utils import exp, log
@@ -1035,7 +1036,38 @@ def Pbubble_mixture(T=None, zs=None, Psats=None, CASRNs=None,
     return Pbubble
 
 
+def bubble_at_P(P, zs, vapor_pressure_eqns, fugacities=None, gammas=None):
+    '''Calculates bubble point for a given pressure
 
+    Parameters
+    ----------
+    P : float
+        Pressure, [Pa]
+    zs : list[float]
+        Overall mole fractions of all species, [-]
+    vapor_pressure_eqns : list[functions]
+        Temperature dependent function for each specie, Returns Psat, [Pa]
+    fugacities : list[float], optional
+        fugacities of each species, defaults to list of ones[-]
+    gammas : list[float], optional
+        gammas of each species, defaults to list of ones[-]
+
+    Returns
+    -------
+    Tbubble : float, optional
+        Temperature of bubble point at pressure `P`, [K]
+
+    '''
+
+    def bubble_P_error(T):
+        Psats = [VP(T) for VP in vapor_pressure_eqns]
+        Pcalc = bubble_at_T(zs, Psats, fugacities, gammas)
+
+        return P - Pcalc
+
+    T_bubble = newton(bubble_P_error, 300)
+
+    return T_bubble
 
 
 def Pdew_mixture(T=None, zs=None, Psats=None, CASRNs=None,
