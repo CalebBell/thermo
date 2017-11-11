d919adf6111143f9e2d6dde62ec6e921823665aa
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2017-11-11 11:06:56 -0400

    Document the corresponding entropy change

--- thermo/property_package.py
+++ thermo/property_package.py
@@ -410,17 +410,17 @@ class IdealPPThermodynamic(Ideal_PP):
         For a pure gas mixture:
             
         .. math::
-             H = \sum_i z_i \cdot \int_{T_{ref}}^T C_{p}^{ig}(T)
+             H = \sum_i z_i \cdot \int_{T_{ref}}^T C_{p}^{ig}(T) dT
              
         For a pure liquid mixture:
             
         .. math::
-             H = \sum_i z_i \left( \int_{T_{ref}}^T C_{p}^{ig}(T) + H_{vap, i}(T) \right)
+             H = \sum_i z_i \left( \int_{T_{ref}}^T C_{p}^{ig}(T) dT + H_{vap, i}(T) \right)
              
         For a vapor-liquid mixture:
             
         .. math::
-             H = \sum_i z_i \cdot \int_{T_{ref}}^T C_{p}^{ig}(T)
+             H = \sum_i z_i \cdot \int_{T_{ref}}^T C_{p}^{ig}(T) dT
                  + \sum_i x_i\left(1 - \frac{V}{F}\right)H_{vap, i}(T)
 
         Returns
@@ -432,8 +432,8 @@ class IdealPPThermodynamic(Ideal_PP):
         Notes
         -----
         The object must be flashed before this routine can be used. It 
-        depends on the properties T, zs, HeatCapacityGases, 
-        EnthalpyVaporizations, and xs.
+        depends on the properties T, zs, xs, V_over_F, HeatCapacityGases, 
+        EnthalpyVaporizations, and.
         '''
         H = 0
         T = self.T
@@ -454,6 +454,72 @@ class IdealPPThermodynamic(Ideal_PP):
         return H
 
     def entropy_Cpg_Hvap(self):
+        r'''Method to calculate the entropy of an ideal mixture. This routine 
+        is based on "route A", where only the gas heat capacity and enthalpy of
+        vaporization are used.
+        
+        The reference temperature and pressure are properties of the class; it 
+        defaults to 298.15 K and 101325 Pa.
+        
+        There is a contribution due to mixing:
+            
+        .. math::
+            \Delta S_{mixing} = -R\sum_i z_i \log(z_i) 
+            
+        The ideal gas pressure contribution is:
+            
+        .. math::
+            \Delta S_{P} = -R\log\left(\frac{P}{P_{ref}}\right)
+            
+        For a liquid mixture or a partially liquid mixture, the entropy
+        contribution is not so strong - all such pressure effects find that
+        expression capped at the vapor pressure, as shown in [1]_.
+        
+        .. math::
+            \Delta S_{P} = - \sum_i x_i\left(1 - \frac{V}{F}\right) 
+            R\log\left(\frac{P_{sat, i}}{P_{ref}}\right) - \sum_i y_i\left(
+            \frac{V}{F}\right) R\log\left(\frac{P}{P_{ref}}\right)
+            
+        These expressions are combined with the standard heat capacity and 
+        enthalpy of vaporization expressions to calculate the total entropy:
+        
+        For a pure gas mixture:
+            
+        .. math::
+             S = \Delta S_{mixing} + \Delta S_{P} + \sum_i z_i \cdot
+             \int_{T_{ref}}^T \frac{C_{p}^{ig}(T)}{T} dT
+                          
+        For a pure liquid mixture:
+            
+        .. math::
+             S = \Delta S_{mixing} + \Delta S_{P} + \sum_i z_i \left( 
+             \int_{T_{ref}}^T \frac{C_{p}^{ig}(T)}{T} dT + \frac{H_{vap, i}
+             (T)}{T} \right)
+            
+        For a vapor-liquid mixture:
+            
+        .. math::
+             S = \Delta S_{mixing} + \Delta S_{P} + \sum_i z_i \cdot 
+             \int_{T_{ref}}^T \frac{C_{p}^{ig}(T)}{T} dT + \sum_i x_i\left(1
+             - \frac{V}{F}\right)\frac{H_{vap, i}(T)}{T}
+             
+        Returns
+        -------
+        S : float
+            Entropy of the mixture with respect to the reference temperature,
+            [J/mol/K]
+            
+        Notes
+        -----
+        The object must be flashed before this routine can be used. It 
+        depends on the properties T, P, zs, V_over_F, HeatCapacityGases, 
+        EnthalpyVaporizations, VaporPressures, and xs.
+        
+        References
+        ----------
+        .. [1] Poling, Bruce E. The Properties of Gases and Liquids. 5th edition.
+           New York: McGraw-Hill Professional, 2000.
+        '''
         S = 0
         T = self.T
         P = self.P
