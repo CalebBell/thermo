ae8ff182a41c92168f167ef08a28d639cc743052
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2019-10-20 14:57:10 -0600

    Add method to handle constrained limit

---
+++ tests/test_fitting.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+'''Chemical Engineering Design Library (ChEDL). Utilities for process modeling.
+Copyright (C) 2019, Caleb Bell <Caleb.Andrew.Bell@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.'''
+
+from numpy.testing import assert_allclose
+import pytest
+from thermo import fitting
+from thermo.fitting import *
+import os
+import pandas as pd
+
+def test_Twu91_check_params():
+    assert Twu91_check_params((0.694911381318495, 0.919907783415812, 1.70412689631515)) # Ian Bell, methanol
+    assert not Twu91_check_params((0.81000842, 0.94790489, 1.49618907)) # Fit without constraints for methanol
+    
+    # CH4
+    # Twu91_check_params((0.1471, 0.9074, 1.8253))  # Should be consistent - probably a decimal problem
+    assert not  Twu91_check_params((0.0777, 0.9288, 3.0432)) # Should be inconsistent
+    
+    # N2
+    assert Twu91_check_params((0.1240, 0.8897, 2.0138))# consistent
+    assert not Twu91_check_params((0.0760, 0.9144, 2.9857)) # inconsistent
+
+
+def test_Twu91_check_params_Bell():
+    folder = os.path.join(os.path.dirname(fitting.__file__), 'Phase Change')
+    
+    Bell_2018_data = pd.read_csv(os.path.join(folder, 'Bell 2018 je7b00967_si_001.tsv'),
+                                        sep='\t', index_col=6)
+    v = Bell_2018_data_values = Bell_2018_data.values
+    
+    for (c0, c1, c2) in zip(v[:, 2], v[:, 3], v[:, 4]):
+        assert Twu91_check_params((c0, c1, c2))
--- thermo/fitting.py
+++ thermo/fitting.py
@@ -22,7 +22,8 @@ SOFTWARE.'''
 
 from __future__ import division
 
-__all__ = ['alpha_Twu91_objf', 'Twu91_check_params', 'postproc_lmfit']
+__all__ = ['alpha_Twu91_objf', 'alpha_Twu91_objfc',
+           'Twu91_check_params', 'postproc_lmfit']
 
 from cmath import atanh as catanh
 from fluids.numerics import (chebval, brenth, third, sixth, roots_cubic,
@@ -31,6 +32,13 @@ from fluids.numerics import (chebval, brenth, third, sixth, roots_cubic,
                              trunc_exp, secant, linspace, logspace,
                              horner, horner_and_der2)
 from thermo.utils import R
+
+# Supported methods of lmfit
+methods_uncons = ['leastsq', 'least_squares', 'nelder', 'lbfgsb', 'powell', 
+                  'cg', 'cobyla', 'bfgs', 'tnc', 'slsqp', 'ampgo']
+methods_cons = ['differential_evolution', 'brute', 'dual_annealing', 'shgo', 
+                'basinhopping', 'trust-constr', 'emcee']
+
 
 
 def Twu91_check_params(params):
@@ -70,6 +78,76 @@ def alpha_Twu91_objf(params, Trs, alphas_over_a):
     alphas_calc = Trs**(c2*(c1 - 1.0)) * np.exp(c0*(1.0 - Trs**(c1*c2)))
     return alphas_calc - alphas_over_a
 
+
+def alpha_Twu91_objfc(params, Trs, alphas_over_a, debug=False):
+    try:
+        c0, c1, c2 = params['c0'].value, params['c1'].value, params['c2'].value
+    except:
+        c0, c1, c2 = params
+
+    x0 = c1 - 1.0
+    x1 = c2*x0
+    x2 = c1*c2
+    x3 = Trs**x2
+    x4 = Trs**x1*np.exp(-c0*(x3 - 1.0))
+    x5 = c0*x3
+    x6 = c1*x5
+    x7 = c2*x4
+    x8 = x1 - 1.0
+    x9 = x6*(x2*x5 - x2 + 1.0)
+    x10 = 3.0*x1
+    x11 = c2*c2
+    x12 = 3.0*x2
+    x13 = c1*c1*x11
+    
+    Trs_inv = 1.0/Trs
+    Trs_inv2 = Trs_inv*Trs_inv
+    Trs_inv3 = Trs_inv*Trs_inv2
+
+    alphas_calc = d0 = x4
+    d1 = x7*(x0 - x6)*Trs_inv
+    d2 = x7*(x0*x8 - 2.0*x1*x6 + x9)*Trs_inv2
+    d3 = (-x7*(-x0*(x0*x0*x11 - x10 + 2.0) + x10*x6*x8 - x10*x9 
+               + x6*(Trs**(2.0*x2)*c0*c0*x13 + x12*x5 - x12 - 3.0*x13*x5 + x13 + 2.0)
+               )*Trs_inv3)
+    
+    err = np.abs(alphas_calc - alphas_over_a)
+    
+    valid = Twu91_check_params(params)
+    err = alpha_constrain_err(err, d0, d1, d2, d3, valid=valid, debug=debug)
+    return err
+
+
+def alpha_constrain_err(err, d0, d1, d2, d3, valid=True, debug=False):
+    N = len(d0)
+    zeros = np.zeros(N)
+    
+    d0 = np.min([d0, zeros], axis=0)
+    del_0 = np.abs(d0*100)
+    
+    d1 = np.max([d1, zeros], axis=0)
+    del_1 = np.abs(d1*1000)
+
+    d2 = np.min([d2, zeros], axis=0)
+    del_2 = np.abs(d2*5000)
+
+    d3 = np.max([d3, zeros], axis=0)
+    del_3 = np.abs(d3*500000000)
+    
+    tot = np.sum(del_0) + np.sum(del_1) + np.sum(del_2) + np.sum(del_3)
+    if tot == 0 and not valid:
+#        if not Twu91_check_params(params):
+        del_0 = 1e5*np.ones(N)
+    
+    if debug:
+        tot = np.sum(err) + tot
+        print(tot, err, del_0, del_1, del_2, del_3)
+         
+    err += del_0
+    err += del_1
+    err += del_2
+    err += del_3
+    return err
 
 def postproc_lmfit(result):
     result.aard = np.abs(result.residual).sum()/result.ndata
