d7ba76ff47ea2b9bf52a295a0ccb1bfe7f2809fc
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2020-05-18 13:35:56 -0600

    permittivity unit tests updated

--- tests/test_permittivity.py
+++ tests/test_permittivity.py
@@ -20,7 +20,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.'''
 
-from numpy.testing import assert_allclose
+from fluids.numerics import assert_close, assert_close1d, linspace
 import pytest
 import pandas as pd
 import numpy as np
@@ -31,20 +31,53 @@ def test_permittivity_data():
     assert CRC_Permittivity_data.shape == (1303, 9)
 
 
-@pytest.mark.slow
+def test_permittivity_IAPWS():
+    Ts = [238., 256., 273., 273., 323., 323., 373., 373., 510., 523., 614., 647., 673., 673., 773., 773., 873.]
+    rhos = [975.06, 995.25, 999.83, 1180., 988.10,  1258., 958.46, 1110., 15.832, 900., 94.29, 358., 100., 900., 100., 900., 450.]
+    permittivity_calc = [permittivity_IAPWS(T, rho) for T, rho in zip(Ts, rhos)]
+    permittivity_exp = [106.31159697963018, 95.19633650530638, 87.96431108641572, 107.06291112337524, 69.96455480833566, 97.7606839686273, 55.56584297721836, 67.73206302035597, 1.1224589212024803, 32.23229227177932, 1.7702660877662086, 6.194373838662447, 1.7541419715602131, 23.59653593827129, 1.6554135047590008, 20.160449425540186, 6.283091796558804]
+    assert_close1d(permittivity_calc, permittivity_exp)
+
+
 @pytest.mark.meta_T_dept
 def test_Permittivity_class():
     # Test some cases
-    epsilon = Permittivity(CASRN='7732-18-5').T_dependent_property(298.15)
-    assert_allclose(epsilon, 78.35530812232503)
+    water = Permittivity(CASRN='7732-18-5')
+    assert (False, False) == (water.test_property_validity(2000), water.test_property_validity(-10))
+
+    with pytest.raises(Exception):
+        water.test_method_validity(300, 'BADMETHOD' )
+    
+    epsilon = water.T_dependent_property(298.15)
+    assert_close(epsilon, 78.35530812232503)
     assert Permittivity(CASRN='7732-18-5').all_methods == set(['CRC', 'CRC_CONSTANT'])
 
     assert Permittivity(CASRN='132451235-2151234-1234123').all_methods == set()
-    assert None == Permittivity(CASRN='132451235-2151234-1234123').T_dependent_property(300)
+    assert Permittivity(CASRN='132451235-2151234-1234123').T_dependent_property(300) is None
 
     tot_constant = sum([Permittivity(CASRN=i).calculate(T=298.15, method='CRC_CONSTANT') for i in CRC_Permittivity_data.index])
-    assert_allclose(tot_constant, 13526.653700000023)
+    assert_close(tot_constant, 13526.653700000023)
 
+    assert False == Permittivity(CASRN='7732-18-5').test_method_validity(228.15, 'CRC_CONSTANT')
+    assert False == Permittivity(CASRN='7732-18-5').test_method_validity(228.15, 'CRC')
+
+
+
+    # Tabular data
+    w = Permittivity(CASRN='7732-18-5')
+    Ts = linspace(273, 372, 10)
+    permittivities = [87.75556413000001, 83.530500320000016, 79.48208925000003, 75.610330919999996, 71.915225330000013, 68.396772480000024, 65.05497237000003, 61.889825000000044, 58.901330369999997, 56.08948848]
+    w.set_tabular_data(Ts=Ts, properties=permittivities)
+    assert_close(w.T_dependent_property(305.), 75.95500925000006)
+    w.tabular_extrapolation_permitted = True
+    assert_close(w.T_dependent_property(200.), 115.79462395999997)
+    w.tabular_extrapolation_permitted = False
+    assert None == w.T_dependent_property(200.)
+
+@pytest.mark.slow
+@pytest.mark.fuzz
+@pytest.mark.meta_T_dept
+def test_Permittivity_class_fuzz():
     sums_min, sums_avg, sums_max = 0, 0, 0
     for i in CRC_Permittivity_data.index:
         a = Permittivity(CASRN=i)
@@ -52,32 +85,4 @@ def test_Permittivity_class():
             sums_min += a.calculate(a.CRC_Tmin, 'CRC')
             sums_avg += a.calculate((a.CRC_Tmax+a.CRC_Tmin)/2., 'CRC')
             sums_max += a.calculate(a.CRC_Tmax, 'CRC')
-    assert_allclose([sums_min, sums_avg, sums_max], [10582.970609439253, 8312.897581451223, 6908.073524704013])
-
-
-    assert (False, False) == (a.test_property_validity(2000), a.test_property_validity(-10))
-    assert False == Permittivity(CASRN='7732-18-5').test_method_validity(228.15, 'CRC_CONSTANT')
-    assert False == Permittivity(CASRN='7732-18-5').test_method_validity(228.15, 'CRC')
-
-    with pytest.raises(Exception):
-        a.test_method_validity(300, 'BADMETHOD' )
-
-
-    # Tabular data
-    w = Permittivity(CASRN='7732-18-5')
-    Ts = np.linspace(273, 372, 10)
-    permittivities = [87.75556413000001, 83.530500320000016, 79.48208925000003, 75.610330919999996, 71.915225330000013, 68.396772480000024, 65.05497237000003, 61.889825000000044, 58.901330369999997, 56.08948848]
-    w.set_tabular_data(Ts=Ts, properties=permittivities)
-    assert_allclose(w.T_dependent_property(305.), 75.95500925000006)
-    w.tabular_extrapolation_permitted = True
-    assert_allclose(w.T_dependent_property(200.), 115.79462395999997)
-    w.tabular_extrapolation_permitted = False
-    assert None == w.T_dependent_property(200.)
-
-
-def test_permittivity_IAPWS():
-    Ts = [238., 256., 273., 273., 323., 323., 373., 373., 510., 523., 614., 647., 673., 673., 773., 773., 873.]
-    rhos = [975.06, 995.25, 999.83, 1180., 988.10,  1258., 958.46, 1110., 15.832, 900., 94.29, 358., 100., 900., 100., 900., 450.]
-    permittivity_calc = [permittivity_IAPWS(T, rho) for T, rho in zip(Ts, rhos)]
-    permittivity_exp = [106.31159697963018, 95.19633650530638, 87.96431108641572, 107.06291112337524, 69.96455480833566, 97.7606839686273, 55.56584297721836, 67.73206302035597, 1.1224589212024803, 32.23229227177932, 1.7702660877662086, 6.194373838662447, 1.7541419715602131, 23.59653593827129, 1.6554135047590008, 20.160449425540186, 6.283091796558804]
-    assert_allclose(permittivity_calc, permittivity_exp)
+    assert_close1d([sums_min, sums_avg, sums_max], [10582.970609439253, 8312.897581451223, 6908.073524704013])
