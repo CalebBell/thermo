ecbf70a10c96b340e870423b12b832a2a577d5b8
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2020-08-05 00:00:33 -0600

    Fourth

--- thermo/eos.py
+++ thermo/eos.py
@@ -1242,19 +1242,6 @@ class GCEOS(object):
             \partial^2 P}{\partial V^2}\right)_T \left(\frac{\partial P}{
             \partial V}\right)^{-3}_T
 
-            \left(\frac{\partial^2 T}{\partial V^2}\right)_P = -\left[
-            \left(\frac{\partial^2 P}{\partial V^2}\right)_T
-            \left(\frac{\partial P}{\partial T}\right)_V
-            - \left(\frac{\partial P}{\partial V}\right)_T
-            \left(\frac{\partial^2 P}{\partial T \partial V}\right) \right]
-            \left(\frac{\partial P}{\partial T}\right)^{-2}_V
-            + \left[\left(\frac{\partial^2 P}{\partial T\partial V}\right)
-            \left(\frac{\partial P}{\partial T}\right)_V
-            - \left(\frac{\partial P}{\partial V}\right)_T
-            \left(\frac{\partial^2 P}{\partial T^2}\right)_V\right]
-            \left(\frac{\partial P}{\partial T}\right)_V^{-3}
-            \left(\frac{\partial P}{\partial V}\right)_T
-
             \left(\frac{\partial^2 V}{\partial T^2}\right)_P = -\left[
             \left(\frac{\partial^2 P}{\partial T^2}\right)_V
             \left(\frac{\partial P}{\partial V}\right)_T
@@ -1322,7 +1309,7 @@ class GCEOS(object):
            Butterworth-Heinemann, 1985.
         '''
         (dP_dT, dP_dV, dV_dT, dV_dP, dT_dV, dT_dP,
-            d2P_dT2, d2P_dV2, d2V_dT2, d2V_dP2, d2T_dV2, _,
+            d2P_dT2, d2P_dV2, d2V_dT2, d2V_dP2, _, _,
             _, d2P_dTdV, _,
             H_dep, S_dep, Cv_dep) = self.derivatives_and_departures(T, P, V, b, delta, epsilon, a_alpha, da_alpha_dT, d2a_alpha_dT2, quick=quick)
 
@@ -1352,7 +1339,6 @@ class GCEOS(object):
 
             self.d2P_dT2_l, self.d2P_dV2_l = d2P_dT2, d2P_dV2
             self.d2V_dT2_l, self.d2V_dP2_l = d2V_dT2, d2V_dP2
-            self.d2T_dV2_l = d2T_dV2
 
             self.d2P_dTdV_l = d2P_dTdV
 
@@ -1366,8 +1352,6 @@ class GCEOS(object):
 
             self.d2P_dT2_g, self.d2P_dV2_g = d2P_dT2, d2P_dV2
             self.d2V_dT2_g, self.d2V_dP2_g = d2V_dT2, d2V_dP2
-            self.d2T_dV2_g = d2T_dV2
-
             self.d2P_dTdV_g = d2P_dTdV
 
             self.H_dep_g, self.S_dep_g, self.G_dep_g = H_dep, S_dep, G_dep,
@@ -3969,6 +3953,58 @@ class GCEOS(object):
         '''
         dT_dP = self.dT_dP_g
         return -self.d2P_dT2_g*dT_dP*dT_dP*dT_dP # unused
+
+    @property
+    def d2T_dV2_l(self):
+        r'''Second partial derivative of temperature with respect to 
+        volume (constant pressure) for the liquid phase, [K*mol^2/m^6]
+
+        .. math::
+            \left(\frac{\partial^2 T}{\partial V^2}\right)_P = -\left[
+            \left(\frac{\partial^2 P}{\partial V^2}\right)_T
+            \left(\frac{\partial P}{\partial T}\right)_V
+            - \left(\frac{\partial P}{\partial V}\right)_T
+            \left(\frac{\partial^2 P}{\partial T \partial V}\right) \right]
+            \left(\frac{\partial P}{\partial T}\right)^{-2}_V
+            + \left[\left(\frac{\partial^2 P}{\partial T\partial V}\right)
+            \left(\frac{\partial P}{\partial T}\right)_V
+            - \left(\frac{\partial P}{\partial V}\right)_T
+            \left(\frac{\partial^2 P}{\partial T^2}\right)_V\right]
+            \left(\frac{\partial P}{\partial T}\right)_V^{-3}
+            \left(\frac{\partial P}{\partial V}\right)_T
+        '''
+        dT_dP = self.dT_dP_l
+        dT_dP2 = dT_dP*dT_dP
+
+        d2T_dV2 = dT_dP2*(-(self.d2P_dV2_l*self.dP_dT_l - self.dP_dV_l*self.d2P_dTdV_l)
+                   +(self.d2P_dTdV_l*self.dP_dT_l - self.dP_dV_l*self.d2P_dT2_l)*dT_dP*self.dP_dV_l)
+        return d2T_dV2
+
+    @property
+    def d2T_dV2_g(self):
+        r'''Second partial derivative of temperature with respect to 
+        volume (constant pressure) for the gas phase, [K*mol^2/m^6]
+
+        .. math::
+            \left(\frac{\partial^2 T}{\partial V^2}\right)_P = -\left[
+            \left(\frac{\partial^2 P}{\partial V^2}\right)_T
+            \left(\frac{\partial P}{\partial T}\right)_V
+            - \left(\frac{\partial P}{\partial V}\right)_T
+            \left(\frac{\partial^2 P}{\partial T \partial V}\right) \right]
+            \left(\frac{\partial P}{\partial T}\right)^{-2}_V
+            + \left[\left(\frac{\partial^2 P}{\partial T\partial V}\right)
+            \left(\frac{\partial P}{\partial T}\right)_V
+            - \left(\frac{\partial P}{\partial V}\right)_T
+            \left(\frac{\partial^2 P}{\partial T^2}\right)_V\right]
+            \left(\frac{\partial P}{\partial T}\right)_V^{-3}
+            \left(\frac{\partial P}{\partial V}\right)_T
+        '''
+        dT_dP = self.dT_dP_g
+        dT_dP2 = dT_dP*dT_dP
+
+        d2T_dV2 = dT_dP2*(-(self.d2P_dV2_g*self.dP_dT_g - self.dP_dV_g*self.d2P_dTdV_g)
+                   +(self.d2P_dTdV_g*self.dP_dT_g - self.dP_dV_g*self.d2P_dT2_g)*dT_dP*self.dP_dV_g)
+        return d2T_dV2
 
     @property
     def Vc(self):
