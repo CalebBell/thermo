38399adaba7fe4926db8412081489d43444f8403
Yoel <yoelcortes@gmail.com>
2021-04-17 10:41:44 -0500

    remove unused imports

--- thermo/phases/gibbs_excess.py
+++ thermo/phases/gibbs_excess.py
@@ -23,39 +23,14 @@ SOFTWARE.
 '''
 __all__ = ['GibbsExcessLiquid', 'GibbsExcessSolid']
 
-import sys, os
-from math import isinf, isnan, sqrt
+from math import isinf, isnan
 from fluids.constants import R, R_inv
-import fluids.constants
-from fluids.numerics import (horner, horner_and_der, horner_and_der2, horner_log, jacobian, derivative,
-                             poly_fit_integral_value, poly_fit_integral_over_T_value,
+from fluids.numerics import (horner_and_der2, derivative,
                              evaluate_linear_fits, evaluate_linear_fits_d,
-                             evaluate_linear_fits_d2, quadratic_from_f_ders,
-                             newton_system, trunc_log, trunc_exp, newton, secant)
-from chemicals.utils import (log, log10, exp, Cp_minus_Cv, phase_identification_parameter,
-                          isothermal_compressibility, isobaric_expansion, property_mass_to_molar,
-                          Joule_Thomson, speed_of_sound, dxs_to_dns, dns_to_dn_partials,
-                          normalize, hash_any_primitive, rho_to_Vm, Vm_to_rho)
-
-from chemicals.viscosity import mu_IAPWS, mu_air_lemmon
-from chemicals.thermal_conductivity import k_IAPWS
-import chemicals.iapws
-from chemicals.iapws import iapws95_d3Ar_ddelta2dtau, iapws95_d3Ar_ddeltadtau2
-
-from thermo.serialize import arrays_to_lists
-from thermo.coolprop import has_CoolProp
-from thermo.eos import GCEOS, eos_full_path_dict
-from thermo.eos_mix import IGMIX, GCEOSMIX, eos_mix_full_path_dict, eos_mix_full_path_reverse_dict
-from thermo.eos_mix_methods import PR_lnphis_fastest
-
-from thermo.activity import GibbsExcess, IdealSolution
-from thermo.wilson import Wilson
-from thermo.unifac import UNIFAC
-from thermo.regular_solution import RegularSolution
-from thermo.uniquac import UNIQUAC
-
-from thermo.chemical_package import iapws_correlations
-
+                             evaluate_linear_fits_d2,
+                             trunc_exp, secant)
+from chemicals.utils import log, exp
+from thermo.activity import IdealSolution
 from thermo.utils import POLY_FIT
 from thermo.heat_capacity import HeatCapacityGas, HeatCapacityLiquid
 from thermo.volume import VolumeLiquid, VolumeSolid
