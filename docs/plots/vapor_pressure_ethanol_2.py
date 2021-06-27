7e99e02b8365d18cb113869d01c669cfb9b50d73
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2021-06-27 16:31:40 -0600

    More test coverage of bad tabular inputs

--- docs/plots/vapor_pressure_ethanol_2.py
+++ docs/plots/vapor_pressure_ethanol_2.py
@@ -4,4 +4,6 @@ from thermo import *
 from fluids.numerics import logspace
 
 ethanol_psat = VaporPressure(Tb=351.39, Tc=514.0, Pc=6137000.0, omega=0.635, CASRN='64-17-5')
-ethanol_psat.plot_T_dependent_property(Tmin=400, Tmax=500, methods=['COOLPROP', 'WAGNER_MCGARRY', 'DIPPR_PERRY_8E'], pts=30)
+methods = ['WAGNER_MCGARRY', 'DIPPR_PERRY_8E', 'COOLPROP'] if coolprop.has_CoolProp() else ['WAGNER_MCGARRY', 'DIPPR_PERRY_8E']
+
+ethanol_psat.plot_T_dependent_property(Tmin=400, Tmax=500, methods=methods, pts=30)
--- docs/plots/viscosity_water_0.py
+++ docs/plots/viscosity_water_0.py
@@ -8,7 +8,8 @@ water_psat = VaporPressure(Tb=373.124, Tc=647.14, Pc=22048320.0, omega=0.344, CA
 water_mu = ViscosityLiquid(CASRN="7732-18-5", MW=18.01528, Tm=273.15, Tc=647.14, Pc=22048320.0, 
                       Vc=5.6e-05, omega=0.344,method="DIPPR_PERRY_8E", Psat=water_psat,
                       method_P="LUCAS")
+methods = ['COOLPROP','LUCAS'] if coolprop.has_CoolProp() else ['LUCAS']
 try:
-    water_mu.plot_TP_dependent_property(Tmin=400, Pmin=1e5, Pmax=1e8, methods_P=['COOLPROP','LUCAS'], pts=15, only_valid=False)
+    water_mu.plot_TP_dependent_property(Tmin=400, Pmin=1e5, Pmax=1e8, methods_P=methods, pts=15, only_valid=False)
 except:
     pass
