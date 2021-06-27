d093c3ef24f59ab45112829524dc282957bffd21
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2021-06-27 12:38:58 -0600

    Make documentation plots part of the test suite

--- docs/plots/PRTC_volume_error.py
+++ docs/plots/PRTC_volume_error.py
@@ -5,4 +5,4 @@ from fluids.numerics import logspace
 from math import log10
 
 obj = PRTranslatedConsistent(Tc=190.564, Pc=4599000.0, omega=0.008, T=300., P=1e5)
-obj.volume_errors(show=True, plot=True, pts=10)
+obj.volume_errors(show=True, plot=True, pts=7)
