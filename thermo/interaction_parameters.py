724b4f781300e97e99cdf37159ff5022173f9a87
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2021-07-02 13:09:11 -0600

    Prepare for new wilson coefficients

--- thermo/interaction_parameters.py
+++ thermo/interaction_parameters.py
@@ -355,7 +355,9 @@ def load_all_interaction_parameters():
     chemsep_db_path = os.path.join(folder, 'ChemSep')
     ip_files = {'ChemSep PR': os.path.join(chemsep_db_path, 'pr.json'),
                 'ChemSep NRTL': os.path.join(chemsep_db_path, 'nrtl.json'),
-                'ChemSep UNIQUAC': os.path.join(chemsep_db_path, 'uniquac.json')}
+                'ChemSep UNIQUAC': os.path.join(chemsep_db_path, 'uniquac.json'),
+                # 'ChemSep Wilson': os.path.join(chemsep_db_path, 'wilson.json'),
+                }
 
 
     IPDB = InteractionParameterDB()
