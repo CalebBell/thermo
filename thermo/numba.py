6ee05d5c0b87b4d3ff24e4118a9de63a8821df33
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2025-10-18 22:48:28 -0600

    .

--- thermo/numba.py
+++ thermo/numba.py
@@ -111,7 +111,7 @@ def transform_complete_thermo(replaced, __funcs, __all__, normal, vec=False):
     else:
         conv_fun = numba.njit
 
-    chemicals.numba.iapws # Force the transform to occur
+    chemicals.numba.iapws  # noqa: B018 - Force the transform to occur
     for name in dir(chemicals.numba):
         obj = getattr(chemicals.numba, name)
         if isinstance(obj, (CPUDispatcher, ModuleType)):
