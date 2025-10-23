ce06c61a930489d91fce6929f0013848a4d963f7
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2025-10-22 21:46:44 -0600

    .

--- setup.py
+++ setup.py
@@ -197,7 +197,6 @@ classifiers=[
     'Operating System :: Unix',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
